8bc49c2dcdc6f745f26901b1a9795b1c243001b4
bear <x_scorpio@outlook.com>
2019-07-13 20:08:30 +0800

    preg Test

--- preg_api/run_all.py
+++ preg_api/run_all.py
@@ -14,9 +14,15 @@ sys.path.append('./test_case')
 
 
 def creat_test_suit():
+    # 定义一个测试套件
     testunit = unittest.TestSuite()
     test_dir = './test_case'     # 运行时修改为指定测试目录
     print(test_dir)
+    # TestLoader类中提供的方法discover（）
+    # discover（start_dir, pattern ='test *.py', top_level_dir = None ）
+    # start_dir：要测试的模块名或测试用例目录；
+    # pattern='test*.py'：表示用例文件名的匹配原则，下面的例子中匹配文件名为以“test”开头的“.py”文件，星号“*”表示任意多个字符；
+    # top_level_dir=None：测试模块的顶层目录，如果没有顶层目录，默认为None；
     discover = unittest.defaultTestLoader.discover(
         test_dir,
         pattern='test_*.py',)
@@ -32,7 +38,10 @@ def creat_test_suit():
 alltestnames = creat_test_suit()
 
 if __name__ == '__main__':
+    # 按照格式获取当前时间
     now = time.strftime("%Y-%m-%d %H_%M_%S")
+
+    # 定义报告存放的路径
     file = './report/'+now+'_preg_result.html'
     fp = open(file, 'wb')
     runner = HTMLTestRunner(stream=fp,
